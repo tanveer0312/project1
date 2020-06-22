a2d86e9762da409010bfb09cc91f5f3b1ef9fc7c
tanveer0312 <65590271+tanveer0312@users.noreply.github.com>
2020-06-22 23:19:44 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.querySelectorAll(".btn").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 2 , scrollTo:{y:".contact"}});
+    });
+  });
+  
+
+document.querySelectorAll(".c").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 2, scrollTo:{y:".contact"}});
+    });
+  });
+
+  document.querySelectorAll(".a").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 0.5, scrollTo:{y:".about"}});
+    });
+  });
+
+  document.querySelectorAll(".s").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 0.5, scrollTo:{y:".services"}});
+    });
+  });
+
+
+  document.querySelectorAll(".p").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 1, scrollTo:{y:".portfolio"}});
+    });
+  });
+
+  document.querySelectorAll(".back").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      gsap.to(window, {duration: 1.5, scrollTo:{y:".landing"}});
+    });
+  });
+
